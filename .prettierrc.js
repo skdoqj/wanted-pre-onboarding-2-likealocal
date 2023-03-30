ece6039ef8ce1e0a249ffa2c28d62fcde6f44c51
suwan <skdoqj@naver.com>
2023-03-30 16:04:50 +0900

    feature: prettier eslint 설정

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  semi: true,
+  tabWidth: 2,
+  endOfLine: 'auto',
+  trailingComma: 'all',
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  jsxSingleQuote: true,
+};
